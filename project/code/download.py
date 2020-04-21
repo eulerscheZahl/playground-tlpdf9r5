98bcf4d3c64930112524f73a21d4f2e3e5620e57
eulerscheZahl <eulerschezahl@yandex.com>
2020-04-21 17:34:46 +0200

    code cleanup

--- project/code/download.py
+++ project/code/download.py
@@ -2,6 +2,9 @@ import json
 import requests
 
 game_id = 453253378
-r = requests.post('https://www.codingame.com/services/gameResultRemoteService/findByGameId', json = [str(game_id), None])
+r = requests.post(
+	'https://www.codingame.com/services/gameResultRemoteService/findByGameId',
+	json = [str(game_id), None]
+)
 replay = r.json()
 print(replay)
--- statement.md
+++ statement.md
@@ -1,9 +1,4 @@
 # Welcome!
 
-This Python template lets you get started quickly with a simple one-page playground.
-
-```python runnable
-print('Hello World!')
-```
 
 @[Python]({"stubs": ["code/download.py"], "command": "code/download.py"})
